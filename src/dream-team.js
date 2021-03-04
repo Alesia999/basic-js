c9f3367be3809f48f1857600f64cd41922f6269f
Alesia999 <73301282+Alesia999@users.noreply.github.com>
2021-03-05 00:47:24 +0100

    fix: solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+
+module.exports = function createDreamTeam(team) {
+
+return team
+.toString()
+.replace(/\s/g, '')
+.replace(/[0-9]/g, '')
+.replace(',,', ',')
+.replace('[','')
+.split(',')
+.sort()
+.map((word)=>word[0]).join('')
+.toUpperCase()
 };
