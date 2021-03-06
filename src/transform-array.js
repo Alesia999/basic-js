f58d44b63bbfe4930e478291fff170a1c3a0a689
Alesia999 <73301282+Alesia999@users.noreply.github.com>
2021-03-06 16:30:14 +0100

    fix: solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+    newarr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-next') {
+      i++;
+    } else if (arr[i] === '--double-next') {
+      if (i < arr.length - 1) { newarr.push(arr[i + 1]) };
+    } else if (arr[i] === '--discard-prev') {
+      if (i > 0) {
+        newarr.pop(arr[i - 1]);
+      }
+
+    } else if (arr[i] === '--double-prev') {
+      if (i > 0) { newarr.push(arr[i - 1]) };
+    }
+    else {
+      newarr.push(arr[i]);
+    }
+
+  }
+  return newarr;
 };
