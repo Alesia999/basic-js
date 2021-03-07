314aab63a26e3d8649d556f8c9e02d8f40675acb
Alesia999 <73301282+Alesia999@users.noreply.github.com>
2021-03-07 23:43:52 +0100

    fix: solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -4,13 +4,7 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function createDreamTeam(team) {
 
 return team
-.toString()
-.replace(/\s/g, '')
-.replace(/[0-9]/g, '')
-.replace(',,', ',')
-.replace('[','')
-.split(',')
 .sort()
-.map((word)=>word[0]).join('')
-.toUpperCase()
+.map(item => item.slice(0, 1).toUpperCase())
+.join("");
 };
